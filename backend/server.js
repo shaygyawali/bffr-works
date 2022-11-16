78023d5204f521f1178203bd0bcc6b830850695e
Medha Gupta <medha4@gmail.com>
2022-11-16 16:46:10 -0500

    connected to database and added routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const express = require('express')
 
 //express app
 const app = express()
+const mongoose = require('mongoose')
+const usersRoutes = require('./routes/users')
 
 //middleware
 app.use((req, res, next) => {
@@ -12,11 +14,22 @@ app.use((req, res, next) => {
 })
 
 //routes
-app.get('/', (req, res) => {
-	res.json({message: 'Welcome to the App!'})
-})
+// app.get('/', (req, res) => {
+// 	res.json({message: 'Welcome to the App!'})
+// })
 
-//listen for requests
-app.listen(process.env.PORT, () =>{
-	console.log('listening on port 4000!')
-})
+app.use('/api/users', usersRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+	.then(() => {
+		//listen for requests
+		app.listen(process.env.PORT, () =>{
+			console.log('listening on port 4000!')
+		})
+	})
+	.catch((error) => {
+		console.log(error)
+	})
+
+
