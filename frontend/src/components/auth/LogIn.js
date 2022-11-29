86f650aa8b096afffc1851b808edd0a1e1d97eff
Medha Gupta <medha4@gmail.com>
2022-11-29 16:32:28 -0500

    troubleshooting netlify redirects

--- frontend/src/components/auth/LogIn.js
+++ frontend/src/components/auth/LogIn.js
@@ -1,119 +1,130 @@
-import { useState } from 'react';
-import Component from 'react';
+import { useState } from "react";
+import Component from "react";
 import axios from "axios";
-import './LogIn.css';
-import {Routes, Route, useNavigate} from 'react-router-dom'
-import SignUp from './SignUp'
+import "./LogIn.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import SignUp from "./SignUp";
 
-import 'intl-tel-input/build/css/intlTelInput.css';
+import "intl-tel-input/build/css/intlTelInput.css";
 
 function LogIn() {
-   // new line start
-  const [profileData, setProfileData] = useState(null)
-  const [userNumber, setUserNumber] = useState("")
-  const [userName, setUserName] = useState("")
-  const [statusMessage, setStatusMessage] = useState("none")
+  // new line start
+  const [profileData, setProfileData] = useState(null);
+  const [userNumber, setUserNumber] = useState("");
+  const [userName, setUserName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("none");
 
-
-  function updateNumber(evt){
-    setUserNumber(evt.target.value)
-    setStatusMessage("none")
-    console.log(userNumber)
+  function updateNumber(evt) {
+    setUserNumber(evt.target.value);
+    setStatusMessage("none");
+    console.log(userNumber);
   }
 
-  let navigate = useNavigate(); 
-  const routeChange = () =>{
-    console.log(userNumber)
-    if(userNumber.length > 12 || userNumber.length < 12){
-        setStatusMessage("Phone number not valid")
+  let navigate = useNavigate();
+  const routeChange = () => {
+    console.log(userNumber);
+    if (userNumber.length > 12 || userNumber.length < 12) {
+      setStatusMessage("Phone number not valid");
     } else {
-        let path = '../phone-verify'; 
-        navigate(path, {state: {number: '6464622111', name: 'shay'}});
+      let path = "../phone-verify";
+      navigate(path, { state: { number: "6464622111", name: "shay" } });
     }
-  }
-
-  
+  };
 
   function getData() {
     axios({
       method: "GET",
-      url:"/",
+      url: "/",
     })
-    .then((response) => {
-      const res =response.data
-      setProfileData(({
-        profile_name: res.name,
-        about_me: res.about}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
+      .then((response) => {
+        const res = response.data;
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
         }
-    })}
-    //end of new line 
+      });
+  }
+  //end of new line
 
-    const inputProps = {
-      placeholder: 'ReactIntlTelInput',
-    };
-   
-    const intlTelOpts = {
-      preferredCountries: ['cn'],
-    };
-   
-    const value = { iso2: 'cn', dialCode: '86', phone: '12345678901' };
-   
-    const onChange = value => console.log(value);
-    const onReady = (instance, IntlTelInput) => console.log(instance, IntlTelInput);
+  const inputProps = {
+    placeholder: "ReactIntlTelInput",
+  };
+
+  const intlTelOpts = {
+    preferredCountries: ["cn"],
+  };
+
+  const value = { iso2: "cn", dialCode: "86", phone: "12345678901" };
+
+  const onChange = (value) => console.log(value);
+  const onReady = (instance, IntlTelInput) =>
+    console.log(instance, IntlTelInput);
 
   return (
     <div class="App">
+      <span class="dot"></span>
+      <span class="dot2"></span>
+      <span class="dot3"></span>
+      <span class="dot4"></span>
 
-        <span class="dot"></span>
-        <span class="dot2"></span>
-        <span class="dot3"></span>
-        <span class="dot4"></span>
-
-        <div class = "accentBalls">
-          <span class="dot5"></span>
-          <span class="dot6"></span>
-        </div>
-
-
-      <div class="logoHolder">
-          <h1 class = "title"> BFFR </h1>
-          <h2 class = "subtitle"> Friends' Music Now </h2>
+      <div class="accentBalls">
+        <span class="dot5"></span>
+        <span class="dot6"></span>
       </div>
 
-      <div class="mainContainer" >
+      <div class="logoHolder">
+        <h1 class="title"> BFFR </h1>
+        <h2 class="subtitle"> Friends' Music Now </h2>
+      </div>
 
-        <div class="loginContainer"> 
+      <div class="mainContainer">
+        <div class="loginContainer">
           <form>
-            <h2 class = "numberTxt"> Phone Number </h2>
+            <h2 class="numberTxt"> Phone Number </h2>
 
-            <input class = "phoneInput" type="tel" name="name" placeholder="000-000-0000"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12" onChange={evt => updateNumber(evt)}/>
+            <input
+              class="phoneInput"
+              type="tel"
+              name="name"
+              placeholder="000-000-0000"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              maxlength="12"
+              onChange={(evt) => updateNumber(evt)}
+            />
 
-            { (statusMessage != "none") === true ? (<p class = "statusMessage"> {statusMessage} </p>) : null }
+            {(statusMessage != "none") === true ? (
+              <p class="statusMessage"> {statusMessage} </p>
+            ) : null}
 
-
-            <button class = "submitButton" type="submit" onClick={routeChange} value="Log In -->" > Log In --> </button>
+            <button
+              class="submitButton"
+              type="submit"
+              onClick={routeChange}
+              value="Log In -->"
+            >
+              {" "}
+              Log In{" "}
+            </button>
             <p value={userNumber}></p>
           </form>
         </div>
 
-       
-          <h2 class = "createAccountTxt"> 
-            <a href={'sign-up'} style={{textDecoration: 'none'}}>
-          New here? Sign Up
-            </a>
-          </h2>
-
+        <h2 class="createAccountTxt">
+          <a href={"sign-up"} style={{ textDecoration: "none" }}>
+            New here? Sign Up
+          </a>
+        </h2>
       </div>
-
     </div>
 
-   /* <div>
+    /* <div>
       <center>
       <h1>BFFR</h1><br></br>
       <ul>
@@ -130,7 +141,7 @@ function LogIn() {
       </center>
     </div>
 */
-  )
+  );
 }
 
-export default LogIn
+export default LogIn;
